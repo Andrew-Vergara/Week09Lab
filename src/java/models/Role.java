39e5df2efb8a6165222253646284365cdf2b0ce2
Andrew <redma@DESKTOP-CBGG122.cg.shawcable.net>
2020-11-08 12:31:22 -0700

    updated role class to utilize jpa and have sql statements

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -6,30 +6,47 @@
 package models;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 
-/**
- *
- * @author 831690
- */
+@Entity
+@Table(name = "role")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r")
+    , @NamedQuery(name = "Role.findByRoleId", query = "SELECT r FROM Role r WHERE r.roleId = :roleId")
+    , @NamedQuery(name = "Role.findByRoleName", query = "SELECT r FROM Role r WHERE r.roleName = :roleName")})
 public class Role implements Serializable {
-
-    private int roleID;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "role_id")
+    private Integer roleId;
+    @Basic(optional = false)
+    @Column(name = "role_name")
     private String roleName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "role", fetch = FetchType.EAGER)
+    private List<User> userList;
 
     public Role() {
     }
 
-    public Role(int roleID, String roleName) {
-        this.roleID = roleID;
+    public Role(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Role(Integer roleId, String roleName) {
+        this.roleId = roleId;
         this.roleName = roleName;
     }
 
-    public int getRoleID() {
-        return roleID;
+    public int getRoleId() {
+        return roleId;
     }
 
-    public void setRoleID(int roleID) {
-        this.roleID = roleID;
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
     }
 
     public String getRoleName() {
@@ -40,4 +57,36 @@ public class Role implements Serializable {
         this.roleName = roleName;
     }
 
+    @XmlTransient
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (roleId != null ? roleId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Role)) {
+            return false;
+        }
+        Role other = (Role) object;
+        if ((this.roleId == null && other.roleId != null) || (this.roleId != null && !this.roleId.equals(other.roleId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "models.Role[ roleId=" + roleId + " ]";
+    }
 }
